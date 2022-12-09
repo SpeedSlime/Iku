f7a8eb718db8be3155ffe67521ced8e1ce245e1b
Etwodev <etwodev@protonmail.ch>
2022-12-09 12:25:06 +0000

    Fix routes

--- hotupdate/path.go
+++ hotupdate/path.go
@@ -1,12 +1,13 @@
 package hotupdate
 
 import (
+	asahi "github.com/SpeedSlime/Asahi"
 	db "github.com/SpeedSlime/Asahi/database"
 	"github.com/SpeedSlime/Asahi/router"
 )
 
 func Routes() []router.Route {
-	db.Create(&XVersion{})
+	asahi.Handle(db.Create(XVersion{}), "hotupdate.Routes")
 	return []router.Route{
 		router.NewGetRoute("/hotupdate/{device}/version", true, false, HotUpdateVersionGetRoute),
 		router.NewGetRoute("/hotupdate/{device}/assets/{file}", true, false, HotUpdateDownloadGetRoute),
